21e2c3791e5845250da6431a16605afe06a95f81
JulieBodette <jr3python@gmail.com>
2026-04-30 12:15:03 -0400

    Blank Wisej application

---
+++ ShoppingApp/Page1.cs
@@ -0,0 +1,13 @@
+﻿using Wisej.Web;
+
+namespace ShoppingApp
+{
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
