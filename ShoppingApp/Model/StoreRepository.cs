859b1afc41a41952eedb437fc3c1915ceacf7642
JulieBodette <jr3python@gmail.com>
2026-05-01 10:19:46 -0400

    moved list into repository, added add and delete methods

---
+++ ShoppingApp/Model/StoreRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using Wisej.Web;
+
+namespace ShoppingApp.Model
+{
+
+    public interface IStoreRepository
+    {
+        IList<Product> GetProducts();
+        void AddProduct(Product product);
+        void RemoveProduct(int productId);
+    }
+    public class StoreRepository : IStoreRepository
+    {
+        private readonly List<Product> storeStockList;
+        public StoreRepository()
+        {
+            Image apple = Image.FromFile(Application.MapPath("Images/apple.png"));
+            Image banana = Image.FromFile(Application.MapPath("Images/banana.jpg"));
+            Image orange = Image.FromFile(Application.MapPath("Images/orange.png"));
+            storeStockList = new List<Product>()
+            {
+                new Product(1, "apple", apple, 1.99m),
+                new Product(1, "banana", banana, .40m),
+                new Product(1, "orange", orange, 2.80m)
+            };
+        }
+
+        public IList<Product> GetProducts()
+        {
+            return storeStockList;
+        }
+
+        public void AddProduct(Product product)
+        {
+            storeStockList.Add(product);
+        }
+
+        public void RemoveProduct(int productId)
+        {
+            var product = storeStockList.FirstOrDefault(p => p.Id == productId);
+
+            if (product != null)
+                storeStockList.Remove(product);
+        }
+
+    }
+}
--- ShoppingApp/PageView.cs
+++ ShoppingApp/PageView.cs
@@ -18,15 +18,7 @@ namespace ShoppingApp
 
         private void PageView_Load(object sender, System.EventArgs e)
         {
-            storeStockList = new BindingList<Product>();
-            Image apple = Image.FromFile(Application.MapPath("Images/apple.png"));
-            storeStockList.Add(new Product(1, "apple", apple, 1.99m));
             
-            Image banana = Image.FromFile(Application.MapPath("Images/banana.jpg"));
-            storeStockList.Add(new Product(1, "banana", banana, .40m));
-            
-            Image orange = Image.FromFile(Application.MapPath("Images/orange.png"));
-            storeStockList.Add(new Product(1, "orange", orange, 2.80m));
 
             dataRepeater1.DataSource = storeStockList;
 
