b6320fffa79378ab44645f66926c68e0f5332c0a
JulieBodette <jr3python@gmail.com>
2026-05-01 10:30:03 -0400

    Create CartRepository.cs

---
+++ ShoppingApp/Model/CartRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingApp.Model
+{
+    //very similar to Store Repository. In a more complex app these would be more different
+    public interface ICartRepository
+    {
+        IList<Product> GetProducts();
+        void AddProduct(Product product);
+        void RemoveProduct(int productId);
+    }
+    public class CartRepository
+    {
+         private readonly List<Product> cartList;
+        public CartRepository()
+        {
+            cartList = new List<Product>();
+        }
+
+        public IList<Product> GetProducts()
+        {
+            return cartList;
+        }
+
+        public void AddProduct(Product product)
+        {
+            cartList.Add(product);
+        }
+
+        public void RemoveProduct(int productId)
+        {
+            var product = cartList.FirstOrDefault(p => p.Id == productId);
+
+            if (product != null)
+                cartList.Remove(product);
+        }
+    }
+    }
+}
